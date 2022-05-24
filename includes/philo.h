5545396bccd9755062a234050575a4685ca7d377
Oussama Labrahmi <olabrahm@e2r5p6.1337.ma>
2022-05-24 14:34:47 +0100

    :gear: init s_state and s_philo structs

--- includes/philo.h
+++ includes/philo.h
@@ -6,7 +6,7 @@
 /*   By: olabrahm <olabrahm@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/18 11:22:04 by olabrahm          #+#    #+#             */
-/*   Updated: 2022/05/18 11:22:32 by olabrahm         ###   ########.fr       */
+/*   Updated: 2022/05/24 14:33:18 by olabrahm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,22 @@
 
 # include <stdio.h>
 # include <pthread.h>
+
+// define a struct called s_state
+typedef struct	s_state
+{
+	int	num_of_philos; // also num of forks
+	int	time_to_die; // in ms
+	int	time_to_eat; // in ms
+	int	time_to_sleep; // in ms
+	int	total_eats; // in ms
+}	t_state;
+
+// define a struct called s_philo
+typedef struct s_philo
+{
+	int	id;
+	int	timestamp;
+}	t_philo;
 
 #endif
