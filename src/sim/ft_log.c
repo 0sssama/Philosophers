c115b47af59358e773c097f57a2b65244218b649
Oussama Labrahmi <olabrahm@e2r10p11.1337.ma>
2022-06-01 15:31:31 +0100

    :gear: fixing 0 not appearing

--- src/sim/ft_log.c
+++ src/sim/ft_log.c
@@ -6,7 +6,7 @@
 /*   By: olabrahm <olabrahm@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/01 11:54:16 by olabrahm          #+#    #+#             */
-/*   Updated: 2022/06/01 15:28:54 by olabrahm         ###   ########.fr       */
+/*   Updated: 2022/06/01 15:30:58 by olabrahm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,5 @@
 
 void	ft_log(char *str, int id, t_state *state)
 {
-	printf("%8.d: %d %s\n", ft_current_time(state), id, str);
+	printf("%8d: %d %s\n", ft_current_time(state), id, str);
 }
